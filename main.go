39c89ca3891e0bebd0809e5000328ce82c70d338
lyonsdpy <lyonsdpy@163.com>
2020-11-20 19:14:55 +0800

    add makefile,fix handler routes

--- main.go
+++ main.go
@@ -16,7 +16,6 @@ import (
 	"phoenix/utils/log"
 	"runtime"
 	"syscall"
-	"time"
 
 	"github.com/kwanhur/go-svc/svc"
 )
@@ -29,10 +28,11 @@ var (
 	logpath  = flagSet.String("log", "./phoenix.log", "日志文件目录结构,默认值为./phoenix.log")
 	pidpath  = flagSet.String("pid", "./phoenix.pid", "pid文件保存路径")
 	version  = flagSet.Bool("version", false, "查看版本信息")
+	help     = flagSet.Bool("help", false, "查看帮助信息")
 	// 编译参数,给定的为默认参数,具体在makefile中赋值
 	Version   = "0.0.0"
 	CommitID  = "0000000"
-	BuildTime = time.Time{}.Format("2006-01-02 15:04")
+	BuildTime = "0001-01-01"
 )
 
 type program struct {
@@ -46,7 +46,6 @@ func (p *program) Init(env svc.Environment) error {
 		dir := filepath.Dir(os.Args[0])
 		return os.Chdir(dir)
 	}
-
 	if !flagSet.Parsed() {
 		flagSet.Parse(os.Args[1:])
 	}
@@ -55,9 +54,13 @@ func (p *program) Init(env svc.Environment) error {
 	conf.Logpath = *logpath
 	conf.LogLevel = *loglevel
 	if *version {
-		fmt.Printf("software version:%s\n", Version)
-		fmt.Printf("commit id:%s\n", CommitID)
-		fmt.Printf("built by %s %s/%s at %s\n", runtime.Version(), runtime.GOOS, runtime.GOARCH, BuildTime)
+		fmt.Printf("VERSION=%s\n", Version)
+		fmt.Printf("COMMIT_ID=%s\n", CommitID)
+		fmt.Printf("BUILD BY %s %s/%s at %s\n", runtime.Version(), runtime.GOOS, runtime.GOARCH, BuildTime)
+		os.Exit(2)
+	}
+	if *help {
+		flagSet.Usage()
 		os.Exit(2)
 	}
 
